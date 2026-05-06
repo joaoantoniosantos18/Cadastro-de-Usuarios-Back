44a29cfbf895431e19043d346ecb69cd14ed164a
joaoantoniosantos18 <joaoantoniomacedo1018@gmail.com>
2026-05-06 00:30:03 -0300

    Ajustando o database.js para que force o Node a usar o DNS do google

--- config/database.js
+++ config/database.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const dns = require('dns')
+
+// Força o Node.js a usar o DNS do Google em vez do DNS do roteador
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 
 const conectarBancoDeDados = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('Conexão com o banco de dados estabelecida com sucesso!');
-    } catch (error) {
-        console.error('Erro ao conectar ao banco de dados:', error);
-        process.exit(1); // Se a conexão falhar, vai encerrar o processo
-    }
-} 
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('✅ MongoDB conectado com sucesso!')
+  } catch (erro) {
+    console.error('❌ Erro ao conectar:', erro.message)
+    process.exit(1)
+  }
+}
 
-module.exports = conectarBancoDeDados;
+module.exports = conectarBancoDeDados
